861e2d02c9f5a7ab481b58fd8d8b91cdade3d1aa
Emmanuel KONZI <maverickkonzinov@gmail.com>
2020-11-07 20:45:27 +0100

    Decommente tests

--- biblio-domain/test/biblio/domain/usecases/enregistrer_livre_test.rb
+++ biblio-domain/test/biblio/domain/usecases/enregistrer_livre_test.rb
@@ -27,33 +27,32 @@ module Biblio
                                                                                                                     date_publication: nil)
         end
         
-        # def test_enregistrer_livre_echec_validation
-        #   demande_creation_livre = Biblio::RequestModels::EnregistrerLivreRequest.new(nil, nil, nil, nil)
-        #
-        #   presenter = Biblio::Presenters::EnregistrerLivrePresenter.new
-        #   enregistrer_livre_view_model = @usecase.execute(demande_creation_livre, presenter)
-        #
-        #   assert_equal enregistrer_livre_view_model.message, "Echec de l'enregistrement du livre"
-        #   assert_equal enregistrer_livre_view_model.erreurs, ["TITRE: Titre absent"]
-        # end
+        def test_enregistrer_livre_echec_validation
+          demande_creation_livre = Biblio::RequestModels::EnregistrerLivreRequest.new(nil, nil, nil, nil)
 
-        # def test_enregistre_livre_echec_repository
-        #   mock = Minitest::Mock.new
-        #   def mock.save(demande); false; end
-        #
-        #   enregistrer_livre_request = Biblio::RequestModels::EnregistrerLivreRequest.new('Le pagne Noir',
-        #                                                                                  'Bernard Dadié', nil, nil)
-        #
-        #   Biblio::Adapters::LivreInMemoryRepository.stub :new, mock do
-        #     @repo = Biblio::Adapters::LivreInMemoryRepository.new
-        #     @usecase = Biblio::Domain::UseCases::EnregistrerLivre.new(@repo)
-        #
-        #     presenter = Domain::Presenters::EnregistrerLivrePresenter.new
-        #     result = @usecase.execute(enregistrer_livre_request, presenter)
-        #     refute result.success?
-        #     assert_equal result.message,'Enregistrement du livre impossible'
-        #   end
-        # end
+          presenter = Biblio::Presenters::EnregistrerLivrePresenter.new
+          enregistrer_livre_view_model = @usecase.execute(demande_creation_livre, presenter)
+
+          assert_equal enregistrer_livre_view_model.message, "Echec de l'enregistrement du livre"
+          assert_equal enregistrer_livre_view_model.erreurs, ["TITRE: Titre absent"]
+        end
+
+        def test_enregistre_livre_echec_repository
+          mock = Minitest::Mock.new
+          def mock.save(demande); false; end
+
+          enregistrer_livre_request = Biblio::RequestModels::EnregistrerLivreRequest.new('Le pagne Noir', 'Bernard Dadié', nil, nil)
+
+          Biblio::Adapters::LivreInMemoryRepository.stub :new, mock do
+            @repo = Biblio::Adapters::LivreInMemoryRepository.new
+            @usecase = Biblio::Domain::UseCases::EnregistrerLivre.new(@repo)
+
+            presenter = Biblio::Presenters::EnregistrerLivrePresenter.new
+            enregistrer_livre_view_model = @usecase.execute(enregistrer_livre_request, presenter)
+            assert_equal enregistrer_livre_view_model.message, "Echec de l'enregistrement du livre"
+            assert_equal enregistrer_livre_view_model.erreurs, ["DB: repository error"]
+          end
+        end
       end
     end
   end
